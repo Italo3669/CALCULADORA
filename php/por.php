953f8d988c270f91a85370c4401ba9ba2e102987
breder09 <gustavobreder2704@gmail.com>
2023-10-25 20:08:24 -0300

    php

---
+++ php/por.php
@@ -0,0 +1,15 @@
+<?php
+
+$por1 = $_POST ['por1'];
+$por2 = $_POST ['por2'];
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$result1 = ($num1 * 100 ) / $num2;
+$result = ($por1 * $por2 ) / 100;
+
+echo "$result";
+
+echo "$result1";
+
+
+
