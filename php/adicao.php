2e28de260abf73cc9571ce91afa5541d68d18fea
Arthur0034 <arthurslvpss@gmail.com>
2023-11-02 20:34:38 -0300

    css

--- php/adicao.php
+++ php/adicao.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../style/adi.css"/>   <!--referencia a pasta do CSS-->
+    <link rel="stylesheet" href="../style/adi.css">   <!--referencia a pasta do CSS-->
     <link rel="icon"  href="../img/c.png"  type="image/x-icon"> <!--favicon icone do navegador-->
     <title>Calculadora Online</title> <!-- titulo do site -->
 
