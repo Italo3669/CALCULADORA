90626f0568ed877caa3bd0126e80fa9c46119f2a
Arthur0034 <arthurslvpss@gmail.com>
2023-11-12 17:15:59 -0300

    css

--- php/adicao.php
+++ php/adicao.php
@@ -19,7 +19,7 @@
 
 </head>
 
-<body style="background-image: linear-gradient(to right,rgb(167, 121, 18),rgb(0, 0, 0))">  <!-- cor na pagina-->
+<body style="background-image: linear-gradient(to right,rgb(57, 91, 56),rgb(30, 107, 81))">  <!-- cor na pagina-->
 
 <form action="" method="POST">
 
