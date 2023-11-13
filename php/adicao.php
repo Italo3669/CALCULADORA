f22d6399cdda610fe9d80e1f1d2de00bdfb177f1
Italo3669 <italo.gamer.3669@gmail.com>
2023-11-13 20:59:34 -0300

    css

--- php/adicao.php
+++ php/adicao.php
@@ -40,7 +40,7 @@
 
     <div>
     <h4 id="inserir">   
-        Insira um número: <input type="number" name="adi1" placeholder="+" id=inserir1>   <!--caixa de resposta da adição-->
+        Insira um número: <input type="number" name="adi1" placeholder="+" id=inserir1><br>   <!--caixa de resposta da adição-->
         Insira outro número: <input type="number" name="adi2" id=inserir2><br> <!--caixa de resposta da adição-->
     </h4>
     <input type="submit" value="enviar" id="botao1">  <!--Botão de enviar--> <br>
