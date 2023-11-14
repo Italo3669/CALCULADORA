365e972610667e4b448ad6e949acd1e632908ae0
breder09 <gustavobreder2704@gmail.com>
2023-11-13 21:16:05 -0300

    .

--- php/adicao.php
+++ php/adicao.php
@@ -44,6 +44,7 @@
         Insira outro número: <input type="number" name="adi2" id=inserir2><br> <!--caixa de resposta da adição-->
     </h4>
     <input type="submit" value="enviar" id="botao1">  <!--Botão de enviar--> <br>
+    <br>
     </div>
 <?php
 
